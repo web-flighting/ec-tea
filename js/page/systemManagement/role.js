597497bea163aea84f8c71dfc8d044c702d6f558
dibaosong <754356197@qq.com>
2019-04-03 00:28:45 +0800

    修改系统管理

--- js/page/systemManagement/role.js
+++ js/page/systemManagement/role.js
@@ -1,5 +1,6 @@
 $(function(){
-    var authEdit = false;
+    var authEdit = false,
+        authDelete = false;
     var columns = [
         {
             field: 'code',
@@ -30,7 +31,7 @@ $(function(){
            
         },
         {
-            field: '',
+            field: 'roleName',
             title: '操作人',
             align: 'center'
         },
@@ -44,21 +45,48 @@ $(function(){
                 if(authEdit){
                     btns += '<button class="btn btn-sm btn-primary edit" type="button"><i class="fa fa-edit"></i> 编辑</button>';
                 };
+                if(authDelete){
+                    btns += ' <button class="delete btn btn-sm btn-danger" type="button"><i class="glyphicon glyphicon-trash"></i> 删除</button>';
+                };
                 return btns;
             },
             events: { //编辑级别
                 'click .edit': function(e, value, row, index){
-                    $('#levelModal .modal-title').html('编辑级别');
-                    $('#levelName').val(row.accountName);
-                    $('#phone').val(row.mobile);
-                    $('#password').val(row.accountName);
-                    $('#levelId').val(row.id);
+                    $('#levelModal .modal-title').html('修改角色');
                     $('#confirm').attr('data-type', 'edit');
                     $('#levelModal').modal();
-
-            $('#form').serializeObject().class_ids = [1,2]
-
-
+                    method.ajax({
+                        'url': 'role/initEditRolePage',
+                        'type': 'post',
+                        'data': {"roleId": row.roleId},
+                        'success': function(res){
+                            var body = res.body;
+                            $('#roleName').val(body.roleName);
+                            $('#roleId').val(row.roleId);
+                            initTree(res.body.menuAuthoritieTree);
+                        } 
+                    });
+                },
+                'click .delete': function(e, value, row, index){
+                    parent.layer.confirm('确认删除此角色吗？', {
+                                
+                    }, function(){
+                        method.ajax({
+                            'url': 'role/deleteRole',
+                            'type': 'post',
+                            'data': {"roleId": row.roleId},
+                            'success': function(res){
+                                parent.layer.closeAll();
+                                searchTable();
+                                layer.alert('删除成功', {
+                                    time: 1000,
+                                    btn: []
+                                });
+                            } 
+                        });
+                    }, function(){
+                        
+                    });
                 }
             }
         }
@@ -66,9 +94,8 @@ $(function(){
     //初始化table
     method.initTableServer({
         'id': 'table',
-        // 'url': 'accountLevel/initAccountLevelPageList',
-        'url': 'data/systemManagement/dataRole.json',
-        'type': 'get',
+        'url': 'role/initRolePageList',
+        'type': 'post',
         'data': [],
         'pageInfoName': 'rolePageInformation',
         'columns': columns,
@@ -80,7 +107,6 @@ $(function(){
             return method.getTableParams(params, data);
         },
         'success': function(res){
-            console.log(res)
             //处理权限
             var authoritys = res.body.authoritys;
             var add = authoritys.filter(function(item){
@@ -94,6 +120,12 @@ $(function(){
             });
             if(edit.length != 0){
                 authEdit = true;
+            };
+            var deleted = authoritys.filter(function(item){
+                return item.authCode == 'Delete';
+            });
+            if(deleted.length != 0){
+                authDelete = true;
             };
         }
     });
@@ -111,45 +143,91 @@ $(function(){
         //$('#table').bootstrapTable('refreshOptions', {queryParams: param});
     };
 
-    //新增级别
+    //新增角色
     $('#add').on('click', function(){
-        $('#levelModal .modal-title').html('新增级别');
-        $('#levelName').val('');
+        $('#levelModal .modal-title').html('新增角色');
+        $('#roleName').val('');
         $('#confirm').attr('data-type', 'add');
         $('#levelModal').modal();
-        $('#levelName').val('');
-        $('#phone').val('');
-        $('#password').val('');
-        $('.icheckbox_square-green').removeClass('checked')
+        method.ajax({
+            'url': 'role/initMenuAuthorityTree',
+            'type': 'post',
+            'success': function(res){
+                initTree(res.body.menuAuthoritieTree);
+            }
+        });
     });
 
-    //级别确定
-    $('#confirm').on('click', function(){
-        
+    function initTree(menuAuthoritieTree){
+        var treeStr = JSON.stringify(menuAuthoritieTree);
+        treeStr = treeStr.replace(/icon/g, 'icons');
+        treeStr = treeStr.replace(/"ecTeaSystemMenus":null/g, '"ecTeaSystemMenuss":null');
+        treeStr = treeStr.replace(/teaAuthorityTreeItems/g, 'ecTeaSystemMenus');
+        treeStr = treeStr.replace(/authName/g, 'title');
+        treeStr = treeStr.replace(/selected/g, 'checked');
+       
+        var treeData = JSON.parse(treeStr);
+        //渲染树形菜单
+        var setting = {
+            'check': {
+                'enable': true,
+                'chkboxType': { "Y": "s", "N": "s" }
+            },
+            'view': {
+                'selectedMulti': false
+            },
+            'data': {
+                'simpleData': {
+                    'enable': true
+                },
+                'key': {
+                    'name': 'title',
+                    'children': 'ecTeaSystemMenus'
+                }
+            }
+        };
+        var tree = $.fn.zTree.init($("#tree"), setting, treeData);
+    };
+    
 
+    //确定
+    $('#confirm').on('click', function(){
         var _this = $(this),
             type = _this.attr('data-type'),
             url = '',
             obj = $('#form').serializeObject(); //表单中的数据
-        // if(!obj.levelName){
-        //     $('#levelName').focus();
-        //     return;
-        // };
-        var a = 0;
-        if(obj.class_ids.length == 1){
-            obj.class_ids = [obj.class_ids]
+        if(!obj.roleName){
+            $('#roleName').focus();
+            return;
+        };
+        var zTree = $.fn.zTree.getZTreeObj("tree"),
+            nodes = zTree.getCheckedNodes(true);;
+        if(nodes.length == 0){
+            layer.tips('请至少选择一个角色权限', '#confirm', {
+                tips: [1, '#18a689'],
+                time: 1500
+            });
+            return;
+        };
+        var menuIds = [],
+            authorityIds = [];
+        $.each(nodes, function(index, item){
+            if(!!item.menuId){
+                menuIds.push(item.menuId);
+            };
+            if(!!item.authId){
+                authorityIds.push(item.authId);
+            };
+        });
+        obj.menuIds = menuIds;
+        obj.authorityIds = authorityIds;
 
-        }
-
-        console.log(obj)
-        return 
-
-        //如果新增级别
+        //如果新增
         if(type == 'add'){
-            delete obj.levelId;
-            url = 'accountLevel/addAccountLevel';
-        }else{ //如果编辑级别
-            url = 'accountLevel/editAccountLevel';
+            delete obj.roleId;
+            url = 'role/addRole';
+        }else{ //如果编辑
+            url = 'role/editRole';
         };
         method.ajax({
           'url': url,
